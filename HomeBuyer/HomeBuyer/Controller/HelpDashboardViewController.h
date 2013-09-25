81b59232960d6e765db7fe68a977d93d0e93c39e
modivinit <modivinit@gmail.com>
2013-09-25 10:54:33 -0700

    Added help screens for dashboard, home, and profile views.
    There will be one help screen for profile, one for home help, and one
    for dashboard help.
    
    The navigation bar needs to be programmed in.

---
+++ HomeBuyer/HomeBuyer/Controller/HelpDashboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelpDashboardViewController.h
+//  HomeBuyer
+//
+//  Created by Vinit Modi on 9/25/13.
+//  Copyright (c) 2013 Kunance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelpDashboardViewController : UIViewController
+
+@end
---
+++ HomeBuyer/HomeBuyer/Controller/HelpDashboardViewController.m
@@ -0,0 +1,38 @@
+//
+//  HelpDashboardViewController.m
+//  HomeBuyer
+//
+//  Created by Vinit Modi on 9/25/13.
+//  Copyright (c) 2013 Kunance. All rights reserved.
+//
+
+#import "HelpDashboardViewController.h"
+
+@interface HelpDashboardViewController ()
+
+@end
+
+@implementation HelpDashboardViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

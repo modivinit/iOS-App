68c504ac980b15d3d80876daef3bb04bb4c95c9b
modivinit <vinitmodi@Vinits-MacBook-Pro.local>
2013-10-22 15:43:03 -0700

    Added reset password and changed font size on main login screen.

---
+++ HomeBuyer/HomeBuyer/Controller/ResetPasswordViewController.m
@@ -0,0 +1,38 @@
+//
+//  ResetPasswordViewController.m
+//  HomeBuyer
+//
+//  Created by Vinit Modi on 10/22/13.
+//  Copyright (c) 2013 Kunance. All rights reserved.
+//
+
+#import "ResetPasswordViewController.h"
+
+@interface ResetPasswordViewController ()
+
+@end
+
+@implementation ResetPasswordViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

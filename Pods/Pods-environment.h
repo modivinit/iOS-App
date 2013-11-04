cb7361fdbd3210a891cb7fc16080d784b5be18a7
Shilpa Modi <shilpa.gundurao@gmail.com>
2013-11-04 14:18:07 -0800

    Adding pods back to git

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,50 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Facebook-iOS-SDK
+#define COCOAPODS_POD_AVAILABLE_Facebook_iOS_SDK
+#define COCOAPODS_VERSION_MAJOR_Facebook_iOS_SDK 3
+#define COCOAPODS_VERSION_MINOR_Facebook_iOS_SDK 9
+#define COCOAPODS_VERSION_PATCH_Facebook_iOS_SDK 0
+
+// HTAutocompleteTextField
+#define COCOAPODS_POD_AVAILABLE_HTAutocompleteTextField
+#define COCOAPODS_VERSION_MAJOR_HTAutocompleteTextField 1
+#define COCOAPODS_VERSION_MINOR_HTAutocompleteTextField 2
+#define COCOAPODS_VERSION_PATCH_HTAutocompleteTextField 6
+
+// MBProgressHUD
+#define COCOAPODS_POD_AVAILABLE_MBProgressHUD
+#define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
+#define COCOAPODS_VERSION_MINOR_MBProgressHUD 8
+#define COCOAPODS_VERSION_PATCH_MBProgressHUD 0
+
+// Mixpanel
+#define COCOAPODS_POD_AVAILABLE_Mixpanel
+#define COCOAPODS_VERSION_MAJOR_Mixpanel 2
+#define COCOAPODS_VERSION_MINOR_Mixpanel 1
+#define COCOAPODS_VERSION_PATCH_Mixpanel 0
+
+// PKRevealController
+#define COCOAPODS_POD_AVAILABLE_PKRevealController
+#define COCOAPODS_VERSION_MAJOR_PKRevealController 2
+#define COCOAPODS_VERSION_MINOR_PKRevealController 0
+#define COCOAPODS_VERSION_PATCH_PKRevealController 1
+
+// TSCurrencyTextField
+#define COCOAPODS_POD_AVAILABLE_TSCurrencyTextField
+#define COCOAPODS_VERSION_MAJOR_TSCurrencyTextField 0
+#define COCOAPODS_VERSION_MINOR_TSCurrencyTextField 1
+#define COCOAPODS_VERSION_PATCH_TSCurrencyTextField 0
+
+// TSCurrencyTextField/Core
+#define COCOAPODS_POD_AVAILABLE_TSCurrencyTextField_Core
+#define COCOAPODS_VERSION_MAJOR_TSCurrencyTextField_Core 0
+#define COCOAPODS_VERSION_MINOR_TSCurrencyTextField_Core 1
+#define COCOAPODS_VERSION_PATCH_TSCurrencyTextField_Core 0
+
